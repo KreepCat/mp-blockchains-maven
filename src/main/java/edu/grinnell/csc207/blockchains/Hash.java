8f64fcfd267b8063be8b43c5d9cd02ca255fc4af
AlexPollock <pollocka@grinnell.edu>
2024-11-16 15:36:29 -0600

    Added Hash

--- src/main/java/edu/grinnell/csc207/blockchains/Hash.java
+++ src/main/java/edu/grinnell/csc207/blockchains/Hash.java
@@ -1,16 +1,22 @@
 package edu.grinnell.csc207.blockchains;
 
+import java.lang.reflect.Array;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 /**
  * Encapsulated hashes.
  *
- * @author Your Name Here
+ * @author Alex Pollock
+ * @author Kevin Tang
  * @author Samuel A. Rebelsky
  */
 public class Hash {
   // +--------+------------------------------------------------------
   // | Fields |
   // +--------+
-
+  private byte[] bytes;
   // +--------------+------------------------------------------------
   // | Constructors |
   // +--------------+
@@ -18,11 +24,10 @@ public class Hash {
   /**
    * Create a new encapsulated hash.
    *
-   * @param data
-   *   The data to copy into the hash.
+   * @param data The data to copy into the hash.
    */
   public Hash(byte[] data) {
-    // STUB
+    this.bytes = data;
   } // Hash(byte[])
 
   // +---------+-----------------------------------------------------
@@ -35,30 +40,27 @@ public class Hash {
    * @return the number of bytes in the hash.
    */
   int length() {
-    return 0;   // STUB
+    return this.bytes.length;
   } // length()
 
   /**
    * Get the ith byte.
    *
-   * @param i
-   *   The index of the byte to get, between 0 (inclusive) and
-   *   length() (exclusive).
+   * @param i The index of the byte to get, between 0 (inclusive) and length() (exclusive).
    *
    * @return the ith byte
    */
   public byte get(int i) {
-    return 0;   // STUB
+    return this.bytes[i];
   } // get()
 
   /**
-   * Get a copy of the bytes in the hash. We make a copy so that the client
-   * cannot change them.
+   * Get a copy of the bytes in the hash. We make a copy so that the client cannot change them.
    *
    * @return a copy of the bytes in the hash.
    */
   public byte[] getBytes() {
-    return new byte[] {1, 2, 3, 4, 5};      // STUB
+    return Arrays.copyOf(bytes, bytes.length);
   } // getBytes()
 
   /**
@@ -67,21 +69,35 @@ public class Hash {
    * @return the hash as a hex string.
    */
   public String toString() {
-    return "";          // STUB
+    String tmp = new String();
+    StringBuilder toReturn = new StringBuilder();
+    for (int i = 0; i < bytes.length; i++) {
+      toReturn.append(String.format(tmp, Byte.toUnsignedInt(bytes[i])));
+    } // for
+    return toReturn.toString();
   } // toString()
 
   /**
    * Determine if this is equal to another object.
    *
-   * @param other
-   *   The object to compare to.
+   * @param other The object to compare to.
    *
-   * @return true if the two objects are conceptually equal and false
-   *   otherwise.
+   * @return true if the two objects are conceptually equal and false otherwise.
    */
   public boolean equals(Object other) {
-    return false;       // STUB
+    return ((other instanceof Hash) && (this.equals((Hash) other)));
   } // equals(Object)
+
+  /**
+   * Determine if another Hash is equal to this hash
+   *
+   * @param other The Hash to compare to this Hash.
+   *
+   * @return true if the two hashes are structurally equivalent and false otherwise.
+   */
+  public boolean equals(Hash other) {
+    return (other.bytes.equals(this.bytes));
+  } // eqv(Hash)
 
   /**
    * Get the hash code of this object.
